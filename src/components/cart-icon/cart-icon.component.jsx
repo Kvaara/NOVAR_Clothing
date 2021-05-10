127b0c472b456b0b7870dc887310c7dfdbd9f1e3
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-05-10 15:41:59 +0300

    Reselect library implemented as a way to memoize/cache values that are the same (so that we don't have to re-render them everytime). Now the newly implemented cart selectors' return values will be assigned to a components props.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,12 +3,13 @@ import { connect } from "react-redux";
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions.js";
 import { hoverCartVisible } from "../../redux/cart/cart.actions.js";
+import { cartItemsCountSelector } from "../../redux/cart/cart.selectors.js";
 
 import { ReactComponent as CartIconImg } from "../../assets/shopping-bag-5.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden, hoverCartVisible }) => {
+const CartIcon = ({ toggleCartHidden, hoverCartVisible, itemCount }) => {
   return (
     <div
       className="cart-icon"
@@ -17,7 +18,7 @@ const CartIcon = ({ toggleCartHidden, hoverCartVisible }) => {
       onMouseLeave={hoverCartVisible}
     >
       <CartIconImg className="shopping-icon"></CartIconImg>
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
@@ -27,4 +28,8 @@ const mapDispatchToProps = (dispatch) => ({
   hoverCartVisible: () => dispatch(hoverCartVisible()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+  itemCount: cartItemsCountSelector(state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
