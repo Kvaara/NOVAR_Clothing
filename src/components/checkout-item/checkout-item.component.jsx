62f6722154576c79f54de9d8b7f9d4f806ff0c2c
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-05-14 20:32:25 +0300

    Implemented the ability to increase/decrease the quantity of a product in the checkout page. Added a left and right arrow svg elements that track onClick events. Added a little bit of animation when the user hovers the left/right arrow. In addition I increased the font sizing overall in the checkout page.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,21 +1,46 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import {
+  clearItemFromCart,
+  removeItem,
+  addItem,
+} from "../../redux/cart/cart.actions";
 
 import { ReactComponent as RemoveIcon } from "./delete.svg";
+import { ReactComponent as LeftArrow } from "./left_arrow.svg";
+import { ReactComponent as RightArrow } from "./right_arrow.svg";
 
 import "./checkout-item.styles.scss";
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item"></img>
+const CheckoutItem = ({ cartItem, clearItem, removeItem, addItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item"></img>
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          <LeftArrow></LeftArrow>
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          <RightArrow></RightArrow>
+        </div>
+      </span>
+      <span className="price">{price} €</span>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        <RemoveIcon></RemoveIcon>
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price} €</span>
-    <div className="remove-button">
-      <RemoveIcon></RemoveIcon>
-    </div>
-  </div>
-);
+  );
+};
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+  addItem: (item) => dispatch(addItem(item)),
+});
 
-export default CheckoutItem;
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from "./cart.types.js";
-import { addItemToCart } from "./cart.utils.js";
+import { addItemToCart, removeItemFromCart } from "./cart.utils.js";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -23,6 +23,19 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hoverCartVisible: !state.hoverCartVisible,
+      };
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
       };
     default:
       return state;
