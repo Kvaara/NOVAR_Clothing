c4eced4cd47dff47620334249cab2d0b492de9df
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-08-27 06:19:41 +0300

    Fixed the homepage component from the previous experiments.

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -4,13 +4,10 @@ import { HomePageContainer } from "./homepage.styles.jsx";
 
 import Directory from "../../components/directory/directory.component.jsx";
 
-const HomePage = () => {
-  throw Error;
-  return (
-    <HomePageContainer>
-      <Directory></Directory>
-    </HomePageContainer>
-  );
-};
+const HomePage = () => (
+  <HomePageContainer>
+    <Directory></Directory>
+  </HomePageContainer>
+);
 
 export default HomePage;
